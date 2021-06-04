cd737365183ea55b7f27923653512c596859ecb0
shubham <shubham.ingole@neosofttech.com>
2021-06-04 15:44:21 +0530

    gh page

--- app.js
+++ app.js
@@ -16,50 +16,46 @@ let interval;
 let interval1;
 
 io.on("connection", (socket) => {
-    console.log(`New client connected`);
-    if(interval) {
-        clearInterval(interval);
-    }
-    if(interval1) {
-        clearInterval(interval1);
-    }
-    interval1 = setInterval(() => testEmit(socket), 1000);
-    interval = setInterval(() => getApiAndEmit(socket), 1000);
+  console.log(`New client connected`);
+  if (interval) {
+    clearInterval(interval);
+  }
+  if (interval1) {
+    clearInterval(interval1);
+  }
+  interval1 = setInterval(() => testEmit(socket), 1000);
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
 
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-        clearInterval(interval);
-        clearInterval(interval1);
-    });
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+    clearInterval(interval1);
+  });
 
-    socket.on("Test", (res) => {
-        io.emit("Test", res);
-    });
+  socket.on("Test", (res) => {
+    io.emit("Test", res);
+  });
 
-    socket.on('join', (room) => {
-        console.log(`Socket ${socket.id} joining ${room}`);
-        socket.join(room);
-     });
+  socket.on("join", (room) => {
+    console.log(`Socket ${socket.id} joining ${room}`);
+    socket.join(room);
+  });
 
-    socket.on('chat', (data) => {
-        const { message, room } = data;
-        console.log(`msg: ${message}, room: ${room}`);
-        io.to(room).emit('chat', message);
-     });
-
-    
-
+  socket.on("chat", (data) => {
+    const { message, room } = data;
+    console.log(`msg: ${JSON.stringify(message)}, room: ${room}`);
+    io.to(room).emit("chat", message);
+  });
 });
 
-const getApiAndEmit = socket => {
-    const response = new Date();
-    socket.emit("FromAPI", response);
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  socket.emit("FromAPI", response);
 };
 
-const testEmit = socket => {
-    const data = "Welcome";
-    socket.emit("someEvent", data);
+const testEmit = (socket) => {
+  const data = "Welcome";
+  socket.emit("someEvent", data);
 };
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
-

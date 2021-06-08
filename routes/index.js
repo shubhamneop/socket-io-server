82f409af04a0eeb5fd617fa8e9386776c0af8dba
shubham <shubham.ingole@neosofttech.com>
2021-06-08 17:14:31 +0530

    header

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send({response: "I am alive"}).status(200);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  res.send({ response: "I am alive" }).status(200);
 });
 
 module.exports = router;
